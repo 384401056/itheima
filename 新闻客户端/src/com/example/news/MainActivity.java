6588179b493bc3f126263b8b205fdc8c9a0a3d65
gyb <384401056@qq.com>
2015-02-04 13:59:00 +0800

    添加了新闻客户端

---
+++ 新闻客户端/src/com/example/news/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.news;
+
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends SlidingFragmentActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
